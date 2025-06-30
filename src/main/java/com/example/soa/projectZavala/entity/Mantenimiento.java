83941a42fd5e456a081abf4568420dbe35ffc6b4
pk2-xuan <suan_c1@outlook.com>
2025-06-29 20:39:11 -0500

    new entity Mantenimiento

---
+++ src/main/java/com/example/soa/projectZavala/entity/Mantenimiento.java
@@ -0,0 +1,96 @@
+package com.example.soa.projectZavala.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Mantenimiento")
+public class Mantenimiento {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_mantenimiento")
+    private Integer idMantenimiento;
+
+    @ManyToOne
+    @JoinColumn(name = "vehiculo_id", nullable = false)
+    private Vehiculo vehiculo;
+
+    @Column(name = "fecha_programada", nullable = false, length = 50)
+    private String fechaProgramada;
+
+    @Column(nullable = false, length = 5)
+    private String realizado;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @Column(nullable = false, length = 50)
+    private String estado;
+
+    @ManyToOne
+    @JoinColumn(name = "administrador_id", nullable = false)
+    private Administrador administrador;
+
+    
+ // Constructor vacío
+    public Mantenimiento() {
+    }
+
+    // Getters y Setters
+    public Integer getIdMantenimiento() {
+        return idMantenimiento;
+    }
+
+    public void setIdMantenimiento(Integer idMantenimiento) {
+        this.idMantenimiento = idMantenimiento;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public String getFechaProgramada() {
+        return fechaProgramada;
+    }
+
+    public void setFechaProgramada(String fechaProgramada) {
+        this.fechaProgramada = fechaProgramada;
+    }
+
+    public String getRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(String realizado) {
+        this.realizado = realizado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Administrador getAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(Administrador administrador) {
+        this.administrador = administrador;
+    }
+
+}
